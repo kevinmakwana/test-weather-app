6879a88d405dd18a52b9a59308862ff833deeb17
Kevin Makwana <kevinmakwana189@gmail.com>
2023-05-17 02:53:42 +0530

    Basic structure created

---
+++ database/migrations/2023_05_16_211610_create_city_weather_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('city_weathers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('city_id')->constrained()->onDelete('cascade');
+            $table->float('temperature');
+            $table->float('min_temperature');
+            $table->float('max_temperature');
+            $table->float('humidity');
+            $table->float('wind_speed');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('city_weather');
+    }
+};
