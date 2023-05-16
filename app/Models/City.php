efe27bc80b58e292c1540020c91fda78e8fa2295
Kevin Makwana <kevinmakwana189@gmail.com>
2023-05-17 02:54:50 +0530

    city with city weather relationship added

--- app/Models/City.php
+++ app/Models/City.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class City extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'latitude', 'longitude'];
+
+    public function cityWeathers()
+    {
+        return $this->hasMany(CityWeather::class);
+    }
 }
